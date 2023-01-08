099e726b34ec9e98dc71a1e78d0149e01ec10f97
Abbdulbosit <96017917+aabdulbost3@users.noreply.github.com>
2023-01-08 22:00:41 +0500

    responsive_Navbar

--- src/companents/pages/Home/Navbar/Navbar.jsx
+++ src/companents/pages/Home/Navbar/Navbar.jsx
@@ -6,17 +6,35 @@ import Select from '../../../minicomp/select/select'
 function Navbar() {
   const { t, i18n } = useTranslation();
   return (
+    // <div className="container">
+    // <div className='Navbar'>
+    //   <ul>
+    //     <ol><Select/></ol>
+    //     
+    //     
+    //     
+    //     <li><p></p></li>
+    //   </ul>
+    // </div>
+    // </div>
     <div className="container">
-    <div className='Navbar'>
-      <ul>
-        <ol><Select/></ol>
-        <NavLink to={"/"} ><li><p>{t("Navbar.0")}</p></li></NavLink>
-        <NavLink to={"/aboutme"}><li><p>{t("Navbar.1")}</p></li></NavLink>
-        <NavLink to={"/contact"}><li><p>{t("Navbar.2")}</p></li></NavLink>
-        <NavLink to={"/archive"}><li><p>{t("Navbar.3")}</p></li></NavLink>
-      </ul>
+    <section class="top-nav">
+    <div>
+      Logo Here
     </div>
-    </div>
+    <input id="menu-toggle" type="checkbox" />
+    <label class='menu-button-container' for="menu-toggle">
+    <div class='menu-button'></div>
+  </label>
+    <ul class="menu">
+      <li><p><Select/></p></li>
+      <li><NavLink to={"/"} >{t("Navbar.0")}</NavLink></li>
+      <li><NavLink to={"/aboutme"}>{t("Navbar.1")}</NavLink></li>
+      <li><NavLink to={"/contact"}>{t("Navbar.2")}</NavLink></li>
+      <li><NavLink to={"/archive"}>{t("Navbar.3")}</NavLink></li>
+    </ul>
+  </section>
+  </div>
   )
 }
 export default Navbar
