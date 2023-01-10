d8814ccd22641fb608efe0650b9ddbe98842d79a
Abbdulbosit <96017917+aabdulbost3@users.noreply.github.com>
2023-01-11 04:16:55 +0500

    navbar edit style

--- src/companents/pages/Home/Navbar/Navbar.jsx
+++ src/companents/pages/Home/Navbar/Navbar.jsx
@@ -7,20 +7,20 @@ function Navbar() {
   const { t, i18n } = useTranslation();
   return (
     <div className="container">
-    <section class="top-nav">
+    <section className="top-nav">
     <div>
       <h2>A.Abdujalilov</h2>
     </div>
     <p><Select/></p>
     <input id="menu-toggle" type="checkbox" />
-    <label class='menu-button-container' for="menu-toggle">
-    <div class='menu-button'></div>
+    <label className='menu-button-container' for="menu-toggle">
+    <div className='menu-button'></div>
   </label>
-    <ul class="menu">
+    <ul className="menu">
       <li><NavLink to={"/"} >{t("Navbar.0")}</NavLink></li>
       <li><NavLink to={"/aboutme"}>{t("Navbar.1")}</NavLink></li>
       <li><NavLink to={"/contact"}>{t("Navbar.2")}</NavLink></li>
-      <li><NavLink to={"/archive"}>{t("Navbar.3")}</NavLink></li>
+      <li className='poiu'><NavLink to={"/archive"}>{t("Navbar.3")}</NavLink></li>
     </ul>
   </section>
   </div>
